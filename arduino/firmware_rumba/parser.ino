7c750d24d768edd68bdd4d9c4e4ed2b312b8dfad
Dan Royer <dan@marginallyclever.com>
2016-03-24 15:44:44 -0700

    Changed parser_ready() to match Makelangelo and other robots

--- arduino/firmware_rumba/parser.ino
+++ arduino/firmware_rumba/parser.ino
@@ -178,7 +178,7 @@ void parser_processCommand() {
  */
 void parser_ready() {
   sofar=0;  // clear input buffer
-  Serial.print(F("\n>"));  // signal ready to receive input
+  Serial.print(F("\n> "));  // signal ready to receive input
   last_cmd_time = millis();
 }
 
